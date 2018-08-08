2c8cbf3100afbdfaa589e4c6cbaabc4fae450620
titron <hockdtw@163.com>
2018-08-08 13:09:20 +0800

    Duration of static segment in slot is 0.0109ms. Duration of slot in cycle is 0.03ms. => Load Rate = 0.0109/0.03 = 36.33%

--- rh850f1kh-d8_rscanfd_rlin3_rlin2_fr/CANFDGwMcuFirmRH850F1x/src/macros/fr/fr_flxa_0012/inc/cluster.h
+++ rh850f1kh-d8_rscanfd_rlin3_rlin2_fr/CANFDGwMcuFirmRH850F1x/src/macros/fr/fr_flxa_0012/inc/cluster.h
@@ -18,21 +18,56 @@
 #define NML                0        /* NEMC (gNetworkManagementVectorLength) */
 #define TSST               6        /* PRTC1 (gdTSSTransmitter) */
 #define CASM               81       /* PRTC1 (gdCASRxLowMax) */
-#define RXW                301      /* PRTC1 (gdWakeupSymbolRxWindow) */
-#define RXI                59       /* PRTC2 (gdWakeupSymbolRxIdle) */
-#define RXL                55       /* PRTC2 (gdWakeupSymbolRxLow) */
-#define TXI                180      /* PRTC2 (gdWakeupSymbolTxIdle) */
-#define TXL                60       /* PRTC2 (gdWakeupSymbolTxLow) */
+
+#define TEST_LOAD_RATE
+#define RXW                301      /* PRTC1 (gdWakeupSymbolRxWindow) *//* comment @Titron */
+
+#define RXI                59       /* PRTC2 (gdWakeupSymbolRxIdle) *//* comment @Titron */
+#define RXL                55       /* PRTC2 (gdWakeupSymbolRxLow) *//* comment @Titron */
+#define TXI                180      /* PRTC2 (gdWakeupSymbolTxIdle) *//* comment @Titron */
+#define TXL                60       /* PRTC2 (gdWakeupSymbolTxLow) *//* comment @Titron */
+
+//#ifndef TEST_LOAD_RATE
+//#else
+//#define RXW                76      /* PRTC1 (gdWakeupSymbolRxWindow) *//* comment @Titron */
+//
+//#define RXI                14       /* PRTC2 (gdWakeupSymbolRxIdle) */
+//#define RXL                10       /* PRTC2 (gdWakeupSymbolRxLow) *//* comment @Titron */
+//#define TXI                45      /* PRTC2 (gdWakeupSymbolTxIdle) *//* comment @Titron */
+//#define TXL                15       /* PRTC2 (gdWakeupSymbolTxLow) *//* comment @Titron */
+//#endif
+
 #define SFDL               1        /* MHDC (gPayloadLengthStatic) */
-#define MPC                16000    /* GTUC2 (gMacroPerCycle) */
+
+#define MPC                16000    /* GTUC2 (gMacroPerCycle) *//* comment @Titron */
+
+
+#define NIT                15401    /* GTUC4: Network Idle Time Start *//* comment @Titron */
+#define OCS                15501    /* GTUC4 (gOffsetCorrectionStart) *//* comment @Titron */
+//#ifndef TEST_LOAD_RATE
+//#else
+////#define MPC                201    /* GTUC2 (gMacroPerCycle) */
+//
+//#define NIT                195    /* GTUC4: Network Idle Time Start *//* comment @Titron */
+//#define OCS                197    /* GTUC4 (gOffsetCorrectionStart) *//* comment @Titron */
+//#endif
+
 #define SNM                3        /* GTUC2 (gSyncNodeMax) */
-#define NIT                15401    /* GTUC4: Network Idle Time Start */
-#define OCS                15501    /* GTUC4 (gOffsetCorrectionStart) */
-#define SSL                659      /* GTUC7 (gdStaticSlot) */
-#define NSS                18       /* GTUC7 (gNumberOfStaticSlots) */
+
+#ifndef TEST_LOAD_RATE
+#define SSL                659      /* GTUC7 (gdStaticSlot) *//* comment @Titron */
+#define NSS                18       /* GTUC7 (gNumberOfStaticSlots) *//* comment @Titron */
+#else
+#define SSL                15      /* GTUC7 (gdStaticSlot) *//* comment @Titron */
+#define NSS                18       /* GTUC7 (gNumberOfStaticSlots) *//* comment @Titron */
+#endif
+
+
 #define MSL                0        /* GTUC8 (gdMinislot) */
 #define NMS                0        /* GTUC8 (gNumberOfMinislots) */
-#define APO                4        /* GTUC9 (gdActionPointOffset) */
+
+#define APO                4        /* GTUC9 (gdActionPointOffset) *//* comment @Titron */
+
 #define MAPO               0        /* GTUC9 (gdMinislotActionPointOffset) */
 #define DSI                0        /* GTUC9 (gdDynamicSlotIdlePhase) */
 
